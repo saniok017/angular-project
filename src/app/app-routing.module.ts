dabdd3977f861e8b4daa8390a3d5ed68352b85d2
saniok017 <saniok017@gmail.com>
2019-07-02 17:21:06 +0300

    feat: - make tham lazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', loadChildren: () => import('./registration/registration.component').then(mod => mod.RegistrationComponent)},
+  { path: 'login', loadChildren: () => import('./login/login.component').then(mod => mod.LoginComponent)},
+  { path: 'main', loadChildren: () => import('./main/main.component').then(mod => mod.MainComponent) },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
